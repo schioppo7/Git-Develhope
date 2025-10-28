e2cf149def2438e5b38baec6df5f36eef9077936
schioppolone <davideulizi@libero.it>
2025-10-28 14:58:52 +0100

    Ereditarietà - 2

---
+++ Java/Java Basics/Ereditarieta - 2/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Forma areaforma = new Forma();
+        areaforma.calcolaArea();
+
+        Rettangolo rettangolo = new Rettangolo(7.7, 5.5);
+        rettangolo.calcolaArea();
+    }
+}
---
+++ Java/Java Basics/Ereditarieta - 2/src/Rettangolo.java
@@ -0,0 +1,15 @@
+class Rettangolo extends Forma {
+    private double base;
+    private double altezza;
+
+    public Rettangolo(double base, double altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+    @Override
+    public void calcolaArea() {
+        double area = base * altezza;
+        System.out.println("L'area del rettangolo è: " + area);
+    }
+}
+
