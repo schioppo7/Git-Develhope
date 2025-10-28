b21ebff42c59814ca365c70d1da98a3c4c8b181c
schioppolone <davideulizi@libero.it>
2025-10-28 14:25:15 +0100

    Java basics exercise

---
+++ Java/Java Basics/Ereditarietà - 1/src/Animale.java
@@ -0,0 +1,5 @@
+public class Animale {
+    public void faiIlverso() {
+        System.out.println("rrrr");
+    }
+}
---
+++ Java/Java Basics/Ereditarietà - 1/src/Gatto.java
@@ -0,0 +1,6 @@
+public class Gatto extends Animale{
+    @Override
+    public void faiIlverso() {
+        System.out.printf("Mao");
+    }
+}
