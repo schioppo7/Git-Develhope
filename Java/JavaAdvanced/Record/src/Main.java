4dce0962fb2319e470e6c556b5ee20e2e2b1e720
schioppolone <davideulizi@libero.it>
2025-10-30 16:42:02 +0100

    Record

---
+++ Java/JavaAdvanced/Record/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Punto p1 = new Punto(7.7, 14.14);
+        System.out.println(p1);
+    }
+}
+
+
+
+
+
---
+++ Java/JavaAdvanced/Record/src/Punto.java
@@ -0,0 +1,1 @@
+public record Punto(double x, double y) { }
