5a35599e2e5bd134b0d7928e24e15c81b95532b6
schioppolone <davideulizi@libero.it>
2025-11-03 23:02:04 +0100

    DesignPatternsSingletonStatic

--- Java/JavaAdvanced/DesignPatternsSingletonStatic/src/Main.java
+++ Java/JavaAdvanced/DesignPatternsSingletonStatic/src/Main.java
@@ -2,15 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         User user1 = User.getInstance();
-        System.out.println("Default Info = ");
+        System.out.println("Info user1 = ");
         user1.Informazioni();
 
         User user2 = User.getInstance();
-        user2.setNome("Luca");
-        user2.setEta(25);
-
-        System.out.println("Info user2:");
+        user2.setNome("Davide");
+        user2.setEta(19);
+        System.out.println("Info user2 = ");
         user2.Informazioni();
-
     }
 }
