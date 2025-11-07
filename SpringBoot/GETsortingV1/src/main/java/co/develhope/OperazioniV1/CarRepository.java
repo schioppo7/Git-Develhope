0eb9a4c34f8be3c654d68a563cf2042cb09c9944
schioppolone <davideulizi@libero.it>
2025-11-07 11:22:26 +0100

    Sortingv2

--- SpringBoot/GETsortingV1/src/main/java/co/develhope/OperazioniV1/CarRepository.java
+++ SpringBoot/GETsortingV1/src/main/java/co/develhope/OperazioniV1/CarRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
+    Page<Car> findByType(CarType type, Pageable pageable);
 }
