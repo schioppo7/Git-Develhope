c64f55b312e24abc52baae5ce556d1c3a0528023
schioppolone <davideulizi@libero.it>
2025-11-08 16:39:08 +0100

    UnitTestV1

---
+++ SpringBootAdvanced/UnitTestV1/src/test/java/co/develhope/UnitTestV1/UnitTestV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.UnitTestV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
